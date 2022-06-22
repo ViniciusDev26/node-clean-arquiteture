de4b9519ba05ccca45adff4c9e02128c1d5a518f
Carlos Vinicius <viniciusdev.26@gmail.com>
2022-06-22 01:09:34 -0300

    feat: ensure cors is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import { bodyParser } from '../middlewares/body-parser'
+import { cors } from '../middlewares/cors'
 
 export const middlewares = (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
